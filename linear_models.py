b5747653d87126eddb23f1ad72606a8ad79522e5
Jang Tao <jt1310167566@hotmail.com>
2016-09-30 12:58:23 +0800

    init project

--- linear_models.py
+++ linear_models.py
@@ -1,7 +1,6 @@
 # -*-coding: utf-8 -*-
 # Create by Jiang Tao on 2016/9/23
 from sklearn import linear_model
-
 reg = linear_model.LinearRegression()
 reg.fit([[0,0],[1,1],[2,2]],[0,1,2])
-reg.coef_
+print('reg.coef_ =', reg.coef_)
